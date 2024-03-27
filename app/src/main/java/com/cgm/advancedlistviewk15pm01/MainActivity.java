fb3ecd78d08ab60b44eba759c1d0d6e3c3e4221a
CGM <mrmouse.guru@gmail.com>
2024-03-27 08:31:17 +0700

    country simple list view

--- app/src/main/java/com/cgm/advancedlistviewk15pm01/MainActivity.java
+++ app/src/main/java/com/cgm/advancedlistviewk15pm01/MainActivity.java
@@ -3,6 +3,11 @@ package com.cgm.advancedlistviewk15pm01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.NoCopySpan;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<Country> countryList = new ArrayList<>();
+        Country vietnam = new Country("Vietnam", "vn", 98000000);
+        Country usa = new Country("United States", "us", 320000000);
+        Country russia = new Country("Russia", "ru", 142000000);
+
+        countryList.add(vietnam);
+        countryList.add(usa);
+        countryList.add(russia);
+
+        ListView countryListView = findViewById(R.id.listViewId);
+
+        ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(this,
+                android.R.layout.simple_list_item_1, countryList);
+
+        countryListView.setAdapter(adapter);
     }
 }
